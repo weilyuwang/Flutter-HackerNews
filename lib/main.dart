560fe51f199bc76f0bae1460e9dbc604fde252be
weilyuwang <weilyuwang@gmail.com>
2020-05-22 16:17:44 -0400

    scaffold app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
   runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
