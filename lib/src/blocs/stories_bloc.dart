4ddf096b7ef8b95002a48324662c409cb0096fc8
weilyuwang <weilyuwang@gmail.com>
2020-05-23 12:23:24 -0400

    add item stream controller

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -5,10 +5,21 @@ import 'dart:async';
 
 class StoriesBloc {
   final _repository = Repository();
+  // subjects are basically stream controllers created by rxdart lib
+
+  // Exactly like a normal broadcast StreamController with one exception:
+  // this class is both a Stream and Sink.
   final _topIds = PublishSubject<List<int>>();
+
+  // A special StreamController that captures the latest item that has been added to the controller,
+  // and emits that as the first item to any new listener.
+  final _items = BehaviorSubject<int>();
 
   // Getters to Streams
   Stream<List<int>> get topIds => _topIds.stream;
+
+  // Getters to Sinks
+  Function(int) get fetchItem => _items.sink.add;
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -25,8 +36,9 @@ class StoriesBloc {
     );
   }
 
-  // clean up / dispose streams
+  // clean up / dispose streamControllers/subjects
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
