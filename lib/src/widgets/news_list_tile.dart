ba5df49bf48134b092da121b1ffc042dfdd3522c
weilyuwang <weilyuwang@gmail.com>
2020-05-23 12:41:16 -0400

    start to implement news list tile widget

---
+++ lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  const NewsListTile({Key key, this.itemId}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // fetch stories bloc first
+    final bloc = StoriesProvider.of(context);
+    return Container();
+  }
+}
