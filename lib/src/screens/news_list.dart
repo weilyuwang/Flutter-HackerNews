69c7e50bf8d6fea47d4be5aa750d24c9cdba1264
weilyuwang <weilyuwang@gmail.com>
2020-05-22 22:36:56 -0400

    implement stories provider with stories bloc

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,11 +1,38 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Top News')),
-      body: Text('Show some news here'),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 500,
+      itemBuilder: (BuildContext context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData
+                  ? Text('Im visible $index')
+                  : Text('I havent fetched data yet $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(
+      Duration(seconds: 2),
+      () => 'h1',
     );
   }
 }
