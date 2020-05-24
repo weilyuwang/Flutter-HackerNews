9ba649867ada9582f0603a8bf9a695b86ccf8122
weilyuwang <weilyuwang@gmail.com>
2020-05-24 17:41:20 -0400

    start to implement Comments Bloc

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,1 +1,20 @@
-class CommentsBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class CommentsBloc {
+  final _commentsFetcher = PublishSubject<int>();
+  final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
+
+  // Streams
+  Stream<Map<int, Future<ItemModel>>> get itemWithComments =>
+      _commentsOutput.stream;
+
+  // Sink
+  Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
+
+  dispose() {
+    _commentsFetcher.close();
+    _commentsOutput.close();
+  }
+}
