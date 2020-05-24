3fae60b6a8522d4566bd709c1504a6b688d258e1
weilyuwang <weilyuwang@gmail.com>
2020-05-23 22:28:03 -0400

    refactor from ViewList to SliverList

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 import '../blocs/stories_provider.dart';
 import '../widgets/news_list_tile.dart';
 
@@ -13,8 +12,17 @@ class NewsList extends StatelessWidget {
     bloc.fetchTopIds();
 
     return Scaffold(
-      appBar: AppBar(title: Text('Hacker News')),
-      body: buildList(bloc),
+      // appBar: AppBar(title: Text('Hacker News')),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: Text('Hacker News'),
+            floating: true,
+            snap: true,
+          ),
+          buildList(bloc),
+        ],
+      ),
     );
   }
 
@@ -22,16 +30,14 @@ class NewsList extends StatelessWidget {
     return StreamBuilder(
       stream: bloc.topIds,
       builder: (context, AsyncSnapshot<List<int>> snapshot) {
-        if (!snapshot.hasData) {
-          return Center(child: CircularProgressIndicator());
-        }
-
-        return ListView.builder(
-          itemCount: snapshot.data.length,
-          itemBuilder: (BuildContext context, int index) {
-            bloc.fetchItem(snapshot.data[index]);
-            return NewsListTile(itemId: snapshot.data[index]);
-          },
+        return SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              bloc.fetchItem(snapshot.data[index]);
+              return NewsListTile(itemId: snapshot.data[index]);
+            },
+            childCount: snapshot.hasData ? snapshot.data.length : 0,
+          ),
         );
       },
     );
