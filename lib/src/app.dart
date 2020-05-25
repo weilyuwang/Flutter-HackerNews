f55ddb3e9b175a58b30ee79a82c6b3ad6022a383
weilyuwang <weilyuwang@gmail.com>
2020-05-24 22:39:41 -0400

    connect the comments provider with app

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,20 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hackernews/src/screens/news_list.dart';
 import 'blocs/stories_provider.dart';
 import 'screens/news_detail.dart';
+import 'blocs/comments_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoriesProvider(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Hacker News Flutter Demo',
-        theme: ThemeData(
-          appBarTheme: AppBarTheme(
-            color: Colors.orange[800],
+    return CommentsProvider(
+      child: StoriesProvider(
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Hacker News Flutter Demo',
+          theme: ThemeData(
+            appBarTheme: AppBarTheme(
+              color: Colors.orange[800],
+            ),
           ),
+          onGenerateRoute: routes,
         ),
-        onGenerateRoute: routes,
       ),
     );
   }
@@ -28,7 +31,11 @@ class App extends StatelessWidget {
     } else {
       // if '/:id'
       return MaterialPageRoute(builder: (context) {
+        final commentsBloc = CommentsProvider.of(context);
         final itemId = int.parse(settings.name.replaceFirst('/', ''));
+
+        commentsBloc.fetchItemWithComments(itemId);
+
         // and pass into NewsDetail
         return NewsDetail(itemId: itemId);
       });
