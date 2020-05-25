278995b87686aa2c5b1ff4b4c3623ce21ba1ec5e
weilyuwang <weilyuwang@gmail.com>
2020-05-24 22:33:54 -0400

    fetch comment items recursively in the transformer

--- lib/src/blocs/comments_bloc.dart
+++ lib/src/blocs/comments_bloc.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class CommentsBloc {
+  final _repository = Repository();
   final _commentsFetcher = PublishSubject<int>();
   final _commentsOutput = BehaviorSubject<Map<int, Future<ItemModel>>>();
 
@@ -13,8 +16,25 @@ class CommentsBloc {
   // Sink
   Function(int) get fetchItemWithComments => _commentsFetcher.sink.add;
 
+  CommentsBloc() {
+    _commentsFetcher.stream
+        .transform(_commentsTransformer())
+        .pipe(_commentsOutput);
+  }
+
   dispose() {
     _commentsFetcher.close();
     _commentsOutput.close();
+  }
+
+  StreamTransformer<int, Map<int, Future<ItemModel>>> _commentsTransformer() {
+    return ScanStreamTransformer<int, Map<int, Future<ItemModel>>>(
+        (cache, int id, int index) {
+      cache[id] = _repository.fetchItem(id);
+      cache[id].then((ItemModel item) {
+        item.kids.forEach((kidId) => fetchItemWithComments(kidId));
+      });
+      return cache;
+    }, <int, Future<ItemModel>>{});
   }
 }
