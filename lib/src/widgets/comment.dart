b97ac5e3c2a75f5d928d8b55207b8d34a95644f3
weilyuwang <weilyuwang@gmail.com>
2020-05-26 17:56:09 -0400

    use loading container

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hackernews/src/widgets/loading_container.dart';
 import '../models/item_model.dart';
 import 'package:html/parser.dart';
 
@@ -15,7 +16,7 @@ class Comment extends StatelessWidget {
       future: itemMap[itemId],
       builder: (context, AsyncSnapshot<ItemModel> snapshot) {
         if (!snapshot.hasData) {
-          return Text('Still loading comment');
+          return LoadingContainer();
         }
 
         final item = snapshot.data;
